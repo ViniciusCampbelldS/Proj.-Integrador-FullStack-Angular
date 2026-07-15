35135e26df13f84353fbe9592ce1e86bb65ab271
vinicius Campbell dos Santos <vinicius.campbell.etm2018.2@gmail.com>
2026-07-15 19:44:44 -0300

    Node v26.5.0 agngular rodando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  // protected readonly title = signal('Aula de Angular Turma 2026.1');
+  titulo = "Sistema de controle de EPI's";
+  subtitulo = "Gestão de Segurança do Trabalho";
+  descricao = "Aplicação para controle, monitoramento e conformidade de Equipamentos de Proteção Individual (EPI's).";
 }
