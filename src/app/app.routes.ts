2c5d65aa3f8da9b8bb168236268206c561bd973c
vinicius Campbell dos Santos <vinicius.campbell.etm2018.2@gmail.com>
2026-07-17 18:23:52 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { EpiList } from './epi/epi-list/epi-list';
-import { EpiDetalhe } from './epi/epi-detalhe/epi-detalhe';
-import { EpiForm } from './epi/epi-form/epi-form';
-// import { Epifilter } from './epi/epi-filter/epi-filter';
 
-export const routes: Routes = [
-    { path: '', redirectTo: 'epi', pathMatch: 'full' },
-    { path: 'epi', component: EpiList },
-    { path: 'epi/detalhe', component: EpiDetalhe },
-    { path: 'epi/cadastro', component: EpiForm },
-    // { path: 'epi/filtro', component: Epifilter },
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  // protected readonly title = signal('Aula de Angular Turma 2026.1');
-  titulo = "Sistema de controle de EPI's";
-  subtitulo = "Gestão de Segurança do Trabalho";
-  descricao = "Aplicação para controle, monitoramento e conformidade de Equipamentos de Proteção Individual (EPI's).";
+  protected readonly title = signal('Angular');
 }
