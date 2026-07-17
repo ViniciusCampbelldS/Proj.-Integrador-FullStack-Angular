e3f5700813266247441b7ccb311a8eba2e3b1031
vinicius Campbell dos Santos <vinicius.campbell.etm2018.2@gmail.com>
2026-07-17 20:30:01 -0300

    tentativa login 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+// ApplicationConfig é uma interface que define a configuração do aplicativo Angular, incluindo provedores de serviços e interceptadores de erros globais. provideBrowserGlobalErrorListeners é uma função que permite que o Angular capture erros globais do navegador, como erros de rede e erros de script, e exiba mensagens de erro amigáveis para o usuário.
 
 export const appConfig: ApplicationConfig = {
   providers: [
+	// permite que o Angular capture erros globais do navegador, como erros de rede e erros de script, e exiba mensagens de erro amigáveis para o usuário
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+	// permite que o Angular faça o roteamento entre as páginas do aplicativo, como a página de listagem de EPIs, a página de detalhes de um EPI e a página de cadastro de um novo EPI
+    provideRouter(routes),
+	// permite que o Angular faça requisições HTTP para APIs externas, como a API do GitHub e RESTful
+    provideHttpClient()
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { EpiList } from './epi/epi-list/epi-list';
+import { EpiDetalhe } from './epi/epi-detalhe/epi-detalhe';
+import { EpiForm } from './epi/epi-form/epi-form';
+import { LoginComponent } from './auth/login/login';
+// import { Epifilter } from './epi/epi-filter/epi-filter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'epi', pathMatch: 'full' },
+    { path: 'epi', component: EpiList },
+    { path: 'epi/detalhe', component: EpiDetalhe },
+    { path: 'epi/cadastro', component: EpiForm },
+    { path: 'login', component: LoginComponent },
+    // { path: 'epi/filtro', component: Epifilter },
+];
