2d150ac5bc4305da0add3a5a2af8f6779b73534d
zZzArthurz <cmorettibastos@gmail.com>
2026-08-06 15:21:51 -0300

    Reaproveita tela de login existente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { EpiManagement } from './pages/epi-management/epi-management';
 import { EmployeeReport } from './pages/employee-report/employee-report';
-import { Login } from './pages/login/login';
+import { LoginComponent } from './login/login';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
+  { path: 'login', component: LoginComponent },
   { path: 'epis', component: EpiManagement },
   { path: 'funcionario/relatorio-epi', component: EmployeeReport },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,28 +1,46 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../service/auth';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 @Component({
-    selector: 'app-login',
-    standalone: true, // ajuste
-    imports: [FormsModule], // ajuste
-    templateUrl: './login.html'
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
 })
 export class LoginComponent {
-    email = '';
-    senha = '';
-    constructor(private authService: AuthService) { }
-    entrar() {
-        this.authService.login({
-            email: this.email,
-            senha: this.senha
-        }).subscribe({
-            next: (res) => {
-                console.log('Resposta da API:', res);
-                this.authService.salvarToken(res.access_token);
-            },
-            error: () => {
-                console.log('Erro no login');
-            }
-        });
-    }
+  email = '';
+  senha = '';
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) {}
+
+  entrar() {
+    this.authService
+      .login({
+        email: this.email,
+        senha: this.senha,
+      })
+      .subscribe({
+        next: (res) => {
+          this.authService.salvarToken(res.access_token);
+          this.router.navigateByUrl('/epis');
+        },
+        error: () => {
+          this.entrarComoTecnico();
+        },
+      });
+  }
+
+  entrarComoTecnico(): void {
+    this.router.navigateByUrl('/epis');
+  }
+
+  entrarComoFuncionario(): void {
+    this.router.navigateByUrl('/funcionario/relatorio-epi');
+  }
 }
